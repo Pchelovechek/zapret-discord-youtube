5c07076b26ecae77704cafc42ba8cefb597c98ee
Pchelovechek <zyenkomishail@gmail.com>
2024-10-12 20:40:18 +0300

    Update service_discord.bat

--- service_discord.bat
+++ service_discord.bat
@@ -11,7 +11,7 @@ set BIN=%~dp0bin\
 set ARGS=--wf-tcp=443 --wf-udp=443,50000-65535 ^
 --filter-udp=443 --hostlist=\"%~dp0list-discord.txt\" --dpi-desync=fake --dpi-desync-udplen-increment=10 --dpi-desync-repeats=6 --dpi-desync-udplen-pattern=0xDEADBEEF --dpi-desync-fake-quic=\"%BIN%quic_initial_www_google_com.bin\" --new ^
 --filter-udp=50000-65535 --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-fake-quic=\"%BIN%quic_initial_www_google_com.bin\" --new ^
---filter-tcp=443 --hostlist=\"%~dp0list-discord.txt\" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls=\"%BIN%tls_clienthello_www_google_com.bin\"
+--filter-tcp=443 --hostlist=\"%~dp0list-discord.txt\" --dpi-desync=fake,udplen --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls=\"%BIN%tls_clienthello_www_google_com.bin\"
 
 set SRVCNAME=zapret
 
